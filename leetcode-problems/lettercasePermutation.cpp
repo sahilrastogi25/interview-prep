b8e1bbf79c142cb8e378a4eab67718bd9960521c
Sahil <sahil.rstg2598@gmail.com>
2021-07-10 22:46:58 +0530

    updated the letter case permuation solution

--- leetcode-problems/lettercasePermutation.cpp
+++ leetcode-problems/lettercasePermutation.cpp
@@ -5,8 +5,7 @@ public:
             v.push_back(output);
             return;
         }
-        int ch = input[0] - '0';
-        if (ch >= 0 and ch <= 9) {
+        if (isdigit(input[0])) {
             string result = output;
             result.push_back(input[0]);
             input.erase(input.begin());
@@ -15,12 +14,8 @@ public:
         else {
             string op1 = output;
             string op2 = output;
-            op1.push_back(input[0]);
-            if (input[0] <= 90 and input[0] >= 65) {
-                op2.push_back(input[0] + 32);
-            } else if (input[0] > 90) {
-                op2.push_back(input[0] - 32);
-            }
+            op1.push_back(tolower(input[0]));
+            op2.push_back(toupper(input[0]));
             input.erase(input.begin());
             solve(v, input, op1);
             solve(v, input, op2);
