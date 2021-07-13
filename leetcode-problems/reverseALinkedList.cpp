2b5a6312034aa3695aaf284e55bc6323c43561ff
Sahil <sahil.rstg2598@gmail.com>
2021-07-13 14:23:30 +0530

    linked list problems

---
+++ leetcode-problems/reverseALinkedList.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode*temp;
+        while (curr != NULL) {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
