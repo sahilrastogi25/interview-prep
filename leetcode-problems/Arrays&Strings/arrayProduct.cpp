876b43abb2e14ec70d5667e345ee5c942d5bf01f
Sahil <sahil.rstg2598@gmail.com>
2021-07-17 14:31:08 +0530

    updated productArrayExpectSelf

--- leetcode-problems/Arrays&Strings/arrayProduct.cpp
+++ leetcode-problems/Arrays&Strings/arrayProduct.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int>output(n, 0);
+        vector<int>output;
         vector<int>right(n, 1);
         vector<int>left(n, 1);
         for (int i = n - 2; i >= 0; i--) {
