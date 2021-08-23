a43bad251f277a3e2ba4065f5b526c10de858b50
Sahil <sahil.rstg2598@gmail.com>
2021-08-23 11:06:02 +0530

    Made changes to compress string problem

--- leetcode-problems/Arrays&Strings/compressStrings.cpp
+++ leetcode-problems/Arrays&Strings/compressStrings.cpp
@@ -1,15 +1,19 @@
-string compressString(const string &str) {
-	string result;
-	map<char, int>mp;
-	for (char ch : str) {
-		mp[ch]++;
-	}
-	for (auto x : mp) {
-		result += x.first;
-		result += to_string(x.second);
-	}
-	if (result.length() < str.length()) {
-		return result;
-	}
-	return str;
+string encode(string str)
+{
+  int n = str.size();
+  string result;
+  for (int i = 0; i < n; i++) {
+    int cnt = 1;
+    while (i < n - 1 and str[i] == str[i + 1]) {
+      cnt++;
+      i++;
+    }
+    result += str[i];
+    result += to_string(cnt);
+  }
+  //This can happen for a string like abcd --> a1b1c1d1>abcd
+  if (result.size() > str.size()) {
+    return str;
+  }
+  return result;
 }
